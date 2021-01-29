58efc5b04e4a9367ecc4aaec82a84736160a0894
Scott O'Neil <scottieoneil@gmail.com>
2021-01-29 08:51:20 -0800

    test cost calculator function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
-function Pizza(size, toppings, price) {
+function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
-  this.price = price;
+  this.price = 10;
+}
+
+Pizza.prototype.costCalculator = function() {
+  if (this.size === 2) {
+    this.price += 1;
+  } else if (this.size === 3) {
+    this.price += 3;
+  } else if (this.size === 4) {
+    this.price += 6;
+  }
 }
